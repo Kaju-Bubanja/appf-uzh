8d4aa3e282519637f7c2fbbafb035aed80ea1504
Kaju Bubanja <kaju.bubanja@cesag.com>
2020-11-11 07:50:52 +0100

    Added Switzerland to covid script

--- covid.py
+++ covid.py
@@ -10,7 +10,7 @@ import matplotlib.ticker as ticker
 
 # Section 2 - Loading and Selecting Data
 df = pd.read_csv('https://raw.githubusercontent.com/datasets/covid-19/master/data/countries-aggregated.csv', parse_dates=['Date'])
-countries = ['Canada', 'Germany', 'United Kingdom', 'US', 'France', 'China']
+countries = ['Canada', 'Germany', 'United Kingdom', 'US', 'France', 'China', "Switzerland"]
 df = df[df['Country'].isin(countries)]
 
 # Section 3 - Creating a Summary Column
@@ -19,17 +19,17 @@ df['Cases'] = df[['Confirmed', 'Recovered', 'Deaths']].sum(axis=1)
 # Section 4 - Restructuring our Data
 df = df.pivot(index='Date', columns='Country', values='Cases')
 countries = list(df.columns)
-covid = df.reset_index('Date')
-covid.set_index(['Date'], inplace=True)
+covid = df.copy()
+print(df)
 
 # Section 5 - Calculating Rates per 100,000
-populations = {'Canada':37664517, 'Germany': 83721496 , 'United Kingdom': 67802690 , 'US': 330548815, 'France': 65239883, 'China':1438027228}
+populations = {'Canada':37664517, 'Germany': 83721496 , 'United Kingdom': 67802690 , 'US': 330548815, 'France': 65239883, 'China':1438027228, "Switzerland": 8570000}
 percapita = covid.copy()
 for country in list(percapita.columns):
     percapita[country] = percapita[country]/populations[country]*100000
 
 # Section 6 - Generating Colours and Style
-colors = {'Canada':'#045275', 'China':'#089099', 'France':'#7CCBA2', 'Germany':'#FCDE9C', 'US':'#DC3977', 'United Kingdom':'#7C1D6F'}
+colors = {'Canada':'#045275', 'China':'#089099', 'France':'#7CCBA2', 'Germany':'#FCDE9C', 'US':'#DC3977', 'United Kingdom':'#7C1D6F', "Switzerland":"#ff0000"}
 plt.style.use('fivethirtyeight')
 
 # Section 7 - Creating the Visualization
@@ -44,7 +44,6 @@ for country in list(colors.keys()):
     plot.text(x = covid.index[-1], y = covid[country].max(), c = colors[country], s = country, weight = 'bold')
 
 # Section 9 - Adding Labels
-print(covid.index[1])
 plot.text(x = covid.index[1], y = int(covid.max().max())+45000, s = "COVID-19 Cases by Country", fontsize = 23, weight = 'bold', alpha = .75)
 plot.text(x = covid.index[1], y = int(covid.max().max())+15000, s = "For the USA, China, Germany, France, United Kingdom, and Canada\nIncludes Current Cases, Recoveries, and Deaths", fontsize = 16, alpha = .75)
 plot.text(x = percapita.index[1], y = -100000,s = 'datagy.io                      Source: https://github.com/datasets/covid-19/blob/master/data/countries-aggregated.csv', fontsize = 10)
